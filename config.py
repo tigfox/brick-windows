2a9803fb9a31a4299a67a71b3ea38af5cb1af606
Eric Franson <tigfox@gmail.com>
2021-02-20 18:42:21 -0500

    keep splitting

--- config.py
+++ config.py
@@ -14,19 +14,20 @@ data_dir="/mnt/datadisk/"
 
 radio_freq=915.00
 
-rrdCollector = False #simple rrd database, static graphs. great for small collectors, few sensors.
-storage_path = str(data_dir + "rrd/"
+#simple rrd database, static graphs. great for small collectors, few sensors.
+rrdCollector = False
+storage_path = str(data_dir + "rrd/")
 rrd_file = "rrdfile.rrd"
 
-
-influxCollector = True #write to influxdb, either on a large collector or a remote db. fancy.
+#write to influxdb, either on a large collector or a remote db. fancy.
+influxCollector = True
 influx_host = 'localhost'
 influx_port = 8086
 influx_user = 'collector'
 influx_pass = '12 SPACE dicks'
 influx_db = 'lrgcollector'
 
-
+ 
 class Storage:
     def __init__(self):
         if influxCollector:
@@ -66,6 +67,47 @@ class Sensor:
             }, 
             "fields" : { self.type : float(adjusted) }}]
         return data
+
+class Collector:
+    def __init__(self, node_number):
+        # Button A
+        btnA = DigitalInOut(board.D5)
+        btnA.direction = Direction.INPUT
+        btnA.pull = Pull.UP
+         
+        # Button B
+        btnB = DigitalInOut(board.D6)
+        btnB.direction = Direction.INPUT
+        btnB.pull = Pull.UP
+         
+        # Button C
+        btnC = DigitalInOut(board.D12)
+        btnC.direction = Direction.INPUT
+        btnC.pull = Pull.UP
+         
+        # Create the I2C interface.
+        i2c = busio.I2C(board.SCL, board.SDA)
+         
+        # 128x32 built-in OLED Display
+        reset_pin = DigitalInOut(board.D4)
+        display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+        # Clear the display.
+        display.fill(0)
+        display.show()
+        width = display.width
+        height = display.height
+         
+        # Configure Packet Radio
+        CS = DigitalInOut(board.CE1)
+        RESET = DigitalInOut(board.D25)
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
+        rfm69.node = node_number
+        prev_packet = None
+        # Optionally set an encryption key (16 byte AES key). MUST match both
+        # on the transmitter and receiver (or be set to None to disable/the default).
+        rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
+
 
 radio1 = Collector(1)
 radio2 = Sensor(2, "temperature", "Office")
--- radio_rfm69.py
+++ radio_rfm69.py
@@ -12,94 +12,43 @@ Thanks Brent!
 import time
 from random import randint
 import os
-# Import Blinka Libraries
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-import board
-# Import the SSD1306 module.
-import adafruit_ssd1306
-# Import the RFM69 radio module.
-import adafruit_rfm69
-# Import the database tools
-import rrdtool
-
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
- 
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
- 
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
- 
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
- 
-# 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
- 
-# Configure Packet Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
-rfm69.node = 1
-prev_packet = None
-# Optionally set an encryption key (16 byte AES key). MUST match both
-# on the transmitter and receiver (or be set to None to disable/the default).
-rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
-
-# Configure rrdtool database
-# rrdtool.create("test.rrd", "--start", "now", "--step", "30", "RRA:AVERAGE:0.5:1:2880", "DS:2:GAUGE:60:0:100")
 
 # Set some constants for config
 storageDir = "/mnt/datadisk/temps/"
 
-def storeReading(packet):
-    sender = packet[1]
-    data = packet[4:]
-    outputFile = str(storageDir + str(sender) + "/" + str(sender) + ".temps")
-    lockFile = str(storageDir +  str(sender) + "/" + str(sender) + ".lock")
-    rrdfile = str(storageDir + str(sender) + "/" + str(sender) + ".rrd")
-    if os.path.exists(str(storageDir + str(sender) + "/")): # make sure the output directory exists - what do if no?
-        # print("output file: " + outputFile + " exists.")
-        if os.path.exists(lockFile): # make sure the output file isn't being consumed by cron
-            print("lock file exists, waiting")
-            sleepTime = randint(1,500) / 1000
-            print("sleeping to retry: " + str(sleepTime) + " seconds.")
-            time.sleep(sleepTime) # if it is, wait
-            storeReading(packet) # and then retry
-        else: # data file exists, no lock file
-            f = open(outputFile, "a+")
-            # print("opened " + outputFile + " for writing")
-            try:
-                f.write(str(data.decode()) + "\n")
-            except UnicodeDecodeError as e:
-                print("Got a wonky packet: " + str(a))
-            f.close()
-            # print("closed " + outputFile)
-    else:
-        print("We have no output file. Weird packet? Should be: " + outputFile)
-    try:
-        rrdtool.update(rrdfile, "N:" + str(float(packet[4:])))
-    except rrdtool.OperationalError as e:
-        print("rrd db doesn't exist for sensor " + str(sender) + ". Creating new db at " + rrdfile)
-        rrdtool.create(rrdfile, "--start", "now", "--step", "30", "RRA:AVERAGE:0.5:1:2880", "DS:" + str(sender) + ":GAUGE:60:0:100")
-    except ValueError as e:
-        print("rrd db couldn't update: " + str(e))
-    return True
+# def storeReading(packet):
+#     sender = packet[1]
+#     data = packet[4:]
+#     outputFile = str(storageDir + str(sender) + "/" + str(sender) + ".temps")
+#     lockFile = str(storageDir +  str(sender) + "/" + str(sender) + ".lock")
+#     rrdfile = str(storageDir + str(sender) + "/" + str(sender) + ".rrd")
+#     if os.path.exists(str(storageDir + str(sender) + "/")): # make sure the output directory exists - what do if no?
+#         # print("output file: " + outputFile + " exists.")
+#         if os.path.exists(lockFile): # make sure the output file isn't being consumed by cron
+#             print("lock file exists, waiting")
+#             sleepTime = randint(1,500) / 1000
+#             print("sleeping to retry: " + str(sleepTime) + " seconds.")
+#             time.sleep(sleepTime) # if it is, wait
+#             storeReading(packet) # and then retry
+#         else: # data file exists, no lock file
+#             f = open(outputFile, "a+")
+#             # print("opened " + outputFile + " for writing")
+#             try:
+#                 f.write(str(data.decode()) + "\n")
+#             except UnicodeDecodeError as e:
+#                 print("Got a wonky packet: " + str(a))
+#             f.close()
+#             # print("closed " + outputFile)
+#     else:
+#         print("We have no output file. Weird packet? Should be: " + outputFile)
+#     try:
+#         rrdtool.update(rrdfile, "N:" + str(float(packet[4:])))
+#     except rrdtool.OperationalError as e:
+#         print("rrd db doesn't exist for sensor " + str(sender) + ". Creating new db at " + rrdfile)
+#         rrdtool.create(rrdfile, "--start", "now", "--step", "30", "RRA:AVERAGE:0.5:1:2880", "DS:" + str(sender) + ":GAUGE:60:0:100")
+#     except ValueError as e:
+#         print("rrd db couldn't update: " + str(e))
+#     return True
 
 while True:
     packet = None
