de00cb4474ab6d405704151309ca37655c7bd287
tigfox <tigfox@gmail.com>
2021-03-02 21:17:32 -0500

    add water sensor folder

---
+++ radio_sensor_w/radio_sensor_t.ino
@@ -0,0 +1,183 @@
+
+// Modules for the temp sensor
+#include <Wire.h>
+#include "Adafruit_MCP9808.h"
+
+// Create the MCP9808 temperature sensor object
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+// Modules for the radio feather
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <RHReliableDatagram.h>
+#include <math.h>
+
+// Frequency for radio - needs to match receiver.
+#define RF69_FREQ 915.0
+
+// Where to send packets to! This will be the floor receiver
+#define DEST_ADDRESS   1
+// change addresses for each client board, any number :)
+#define MY_ADDRESS     3
+
+#if defined(ADAFRUIT_FEATHER_M0) // Feather M0 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     3
+  #define RFM69_RST     4
+  #define LED           13
+#endif
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+
+void setup() {
+  // First the temp sensor setup:
+  Serial.begin(9600);
+  Serial.println("MCP9808 Initializing");
+    // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x19) for example, also can be left in blank for default address use
+  // Also there is a table with all addres possible for this sensor, you can connect multiple sensors
+  // to the same i2c bus, just configure each sensor with a different address and define multiple objects for that
+  //  A2 A1 A0 address
+  //  0  0  0   0x18  this is the default address
+  //  0  0  1   0x19
+  //  0  1  0   0x1A
+  //  0  1  1   0x1B
+  //  1  0  0   0x1C
+  //  1  0  1   0x1D
+  //  1  1  0   0x1E
+  //  1  1  1   0x1F
+  if (!tempsensor.begin(0x18)) {
+    Serial.println("Couldn't find MCP9808! Check your connections and verify the address is correct.");
+    while (1);
+  }
+    
+   Serial.println("Found MCP9808!");
+     
+   tempsensor.setResolution(1); // sets the resolution mode of reading, the modes are defined in the table bellow:
+   // Mode Resolution SampleTime
+   //  0    0.5°C       30 ms
+   //  1    0.25°C      65 ms
+   //  2    0.125°C     130 ms
+   //  3    0.0625°C    250 ms
+  
+  // Then the radio setup:
+  // Serial.begin(115200);
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+
+  Serial.println("Feather Addressed RFM69 TX Test!");
+  Serial.println();
+
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  
+  if (!rf69_manager.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  rf69.setEncryptionKey(key);
+  
+  pinMode(LED, OUTPUT);
+
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+}
+
+float get_temp() {
+  Serial.println("wake up MCP9808");
+  tempsensor.wake();
+  float cur_temp_c = tempsensor.readTempC();
+  Serial.print("Temp: ");
+  Serial.print(cur_temp_c, 2);
+  Serial.println("*C");
+  tempsensor.shutdown_wake(1);
+  Serial.println("Shutdown temp sensor");
+  return cur_temp_c;
+}
+
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+uint8_t data[] = "  OK";
+
+
+void Blink(byte PIN, byte DELAY_MS, byte loops) {
+  for (byte i=0; i<loops; i++)  {
+    digitalWrite(PIN,HIGH);
+    delay(DELAY_MS);
+    digitalWrite(PIN,LOW);
+    delay(DELAY_MS);
+  }
+}
+
+bool send_packet(char sentype, float temp) {
+  char radiopacket[20];
+  sprintf(radiopacket, "%s:%f",sentype, temp);
+  // dtostrf(temp, 5, 2, radiopacket);
+  // itoa(packetnum++, radiopacket+13, 10);
+  Serial.println(radiopacket);
+  // Serial.print("Sending "); Serial.println(radiopacket);
+
+  // Send a message to the DESTINATION!
+  if (rf69_manager.sendtoWait((uint8_t *)radiopacket, strlen(radiopacket), DEST_ADDRESS)) {
+    // Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;   
+    if (rf69_manager.recvfromAckTimeout(buf, &len, 2000, &from)) {
+      buf[len] = 0; // zero out remaining string
+      
+      Serial.print("Got reply from #"); Serial.print(from);
+      Serial.print(" [RSSI :");
+      Serial.print(rf69.lastRssi());
+      Serial.print("] : ");
+      Serial.println((char*)buf);     
+      Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
+      return true;
+    } else {
+      Serial.println("No reply, is anyone listening?");
+      return false;
+    }
+  } else {
+    Serial.println("Sending failed (no ack)");
+    return false;
+  }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("pulling temp");
+  float cur_temp_c = get_temp();
+  char sensortype = 'T';
+  // Serial.print("temp is ");
+  // Serial.print(cur_temp_c);
+  // Serial.println(" deg C.");
+  send_packet(sensortype, cur_temp_c);
+  delay(10000);
+
+}
